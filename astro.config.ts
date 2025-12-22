b8eab2bd271f61a7e0fe4b8080dc9c947977372d
Tai ZHANG <tai.zhang@outlook.com>
2025-12-22 21:53:37 +0800

    Init commit

---
+++ astro.config.ts
@@ -0,0 +1,21 @@
+// @ts-check
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+const site = process.env.VERCEL
+  ? process.env.VERCEL_ENV === "production"
+    ? "https://astro-shadcn-ui-template.vercel.app"
+    : `https://${process.env.VERCEL_URL}`
+  : (process.env.SITE ?? "http://localhost:4321");
+const base = process.env.BASE || "/";
+
+// https://astro.build/config
+export default defineConfig({
+  site,
+  base,
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
